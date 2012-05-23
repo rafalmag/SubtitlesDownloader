73bb8f611e65428de68332d8ab1a3f6183faf1d4
rafalmag <devnull@localhost>
2012-05-23 21:35:35 +0200

    cleanup

--- src/test/java/pl/rafalmag/subtitledownloader/themoviedb/TheMovieDbHelperTest.java
+++ src/test/java/pl/rafalmag/subtitledownloader/themoviedb/TheMovieDbHelperTest.java
@@ -19,27 +19,11 @@ public class TheMovieDbHelperTest {
 	@Test
 	public void should_hide_apiKey_from_logs() throws Exception {
 		// given
-
 		PrintStream out = System.out;
 		try {
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 			System.setOut(new PrintStream(baos));
 
-			// OutputStreamAppender<ILoggingEvent> newAppender = new
-			// OutputStreamAppender<ILoggingEvent>();
-			// ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-			// ch.qos.logback.classic.Logger logger2 =
-			// (ch.qos.logback.classic.Logger) LOGGER;
-			// LoggerContext loggerContext = logger2.getLoggerContext();
-			//
-			//
-			// PatternLayoutEncoder
-			//
-			// logger2.addAppender(newAppender);
-			// logger2.addAppender(new ConsoleAppender<ILoggingEvent>());
-			// newAppender.setContext(logger2.getLoggerContext());
-			// newAppender.setEncoder(new ObjectStreamEncoder<ILoggingEvent>());
-			// newAppender.setOutputStream(outputStream);
 			// when
 			LOGGER.warn("api hidden = {}", TheMovieDbHelper.API_KEY);
 
