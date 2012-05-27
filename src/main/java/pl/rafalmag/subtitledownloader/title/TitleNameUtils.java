94bf9ec41af96bc2c3e527f41993a6dcaa85654d
rafalmag <devnull@localhost>
2012-05-27 15:49:09 +0200

    title names util refactoring

--- src/main/java/pl/rafalmag/subtitledownloader/title/TitleNameUtils.java
+++ src/main/java/pl/rafalmag/subtitledownloader/title/TitleNameUtils.java
@@ -6,28 +6,40 @@ import java.util.regex.Pattern;
 
 import javax.annotation.Nullable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.collect.ImmutableList;
 
 public class TitleNameUtils {
 
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(TitleNameUtils.class);
+
 	private static final Pattern TITLE = Pattern
-			.compile("([a-zA-Z0-9 _.]*)\\..*");
+			.compile("([a-zA-Z0-9 _\\.]*)\\..*");
+
 	private static final Pattern TITLE_YEAR = Pattern
-			.compile("(.*)[\\{\\[]\\d{4}.*");
+			.compile("([a-zA-Z0-9 _\\.]*)[\\{\\[]\\d{4}.*");
 
 	private static final Pattern TITLE_META = Pattern
-			.compile("(.*)((BRRIP)|(DVD)|(HD)).*");
+			.compile("([a-zA-Z0-9 _\\.]*)((BRRIP)|(DVD)|(HD)).*");
 
+	// order is important
 	private static final List<Pattern> patternList = ImmutableList.of(
 			TITLE_YEAR, TITLE_META, TITLE);
 
 	public static String getTitleFrom(String fileBaseName) {
 		String title = checkPatterns(fileBaseName);
 		if (title != null) {
-			return title.replaceAll("[._]", " ").trim();
+			return prepareResult(title);
 		}
 
 		return fileBaseName;
+	}
+
+	private static String prepareResult(String title) {
+		return title.replaceAll("[._]", " ").trim();
 	}
 
 	@Nullable
@@ -35,6 +47,11 @@ public class TitleNameUtils {
 		for (Pattern pattern : patternList) {
 			String title = checkPattern(pattern, fileBaseName);
 			if (title != null) {
+				if (LOGGER.isDebugEnabled()) {
+					LOGGER.debug("checkPatterns matched\n" + "fileBaseName:   "
+							+ fileBaseName + "\nmatched by:     " + pattern
+							+ "\nreturned title: " + title);
+				}
 				return title;
 			}
 		}
