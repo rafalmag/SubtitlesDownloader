999a8c4a09f3194c68e16aef74f8f8b9374e3b4c
rafalmag <devnull@localhost>
2012-08-30 21:11:29 +0200

    java 7 try-close

--- src/main/java/org/opensubtitles/OpenSubtitlesHasher.java
+++ src/main/java/org/opensubtitles/OpenSubtitlesHasher.java
@@ -27,17 +27,14 @@ public class OpenSubtitlesHasher {
 		long size = file.length();
 		long chunkSizeForFile = Math.min(HASH_CHUNK_SIZE, size);
 
-		FileChannel fileChannel = new FileInputStream(file).getChannel();
-
-		try {
+		try (FileInputStream fileInputStream = new FileInputStream(file);
+				FileChannel fileChannel = fileInputStream.getChannel()) {
 			long head = computeHashForChunk(fileChannel.map(MapMode.READ_ONLY,
 					0, chunkSizeForFile));
 			long tail = computeHashForChunk(fileChannel.map(MapMode.READ_ONLY,
 					Math.max(size - HASH_CHUNK_SIZE, 0), chunkSizeForFile));
 
 			return String.format("%016x", size + head + tail);
-		} finally {
-			fileChannel.close();
 		}
 	}
 
